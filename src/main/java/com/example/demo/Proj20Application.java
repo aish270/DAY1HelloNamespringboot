80603394583c7e0c95d7bc4733a0979f74c65c40
aish270 <eaishwaryalakshmi@gmail.com>
2023-02-21 12:02:18 +0530

    FIRST COMMIT

---
+++ src/main/java/com/example/demo/Proj20Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj20Application.class, args);
+	}
+
+}
